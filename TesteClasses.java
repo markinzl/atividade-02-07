f6ed3fdfd6202dd0a31ff5d87fec0581e6d36237
MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
2024-07-02 07:17:24 -0300

    progamei

---
+++ TesteClasses.java
@@ -0,0 +1,17 @@
+
+import javax.xml.crypto.Data;
+
+import Triangulo;
+
+public class TesteClasses {
+    public static void main(String[] args) {
+        Triangulo t = new Triangulo (2.5f, 3f);
+        float area = (t.base * t.altura)/2;
+        System.out.println("Àrea: " + area);
+
+        Data d1 = new Data(2, 9, 2015);
+        Data d2 = new Data(2, 9, 2015);
+        System.out.println(d1.dia+ "/" + d1.mes+"/"+d1.ano);
+        System.out.println(d2.dia+ "/" + d2.mes+"/"+d2.ano);
+}
+}
---
+++ UsaClasses.java
@@ -0,0 +1,133 @@
+public class UsaClasses {
+    public static void main(String[] args) {
+        //Pessoa p = new Pessoa();
+        Pessoa p;
+        p = new Pessoa();
+        Pessoa p1 = new Pessoa();
+
+        p.nome = "Fulano";
+        p.idade = 25;
+        p.renda = 1000;
+        p.cpf = "523.462.749-00";
+        p.endereco = "Rua Rego Barros";
+        p.peso = 55.5;
+        p.altura = 1.65;
+        p.raca = "Branco";
+        p.datanasc = "14/08/2006";
+        p.nacionalidade = "Brasileiro";
+        p.RG = "52.135.258.07";
+        p.telefone = "11.33456-9559";
+        p.email = "marcusvsa06@gmail,com";
+    
+
+        System.out.println("Nome: " + p.nome);
+        System.out.println("Idade: " + p.idade);
+        System.out.println("Renda: " + p.renda);
+        System.out.println("CPF: " + p.cpf);
+        System.out.println("Endereço: " + p.endereco);
+        System.out.println("Peso: " + p.peso);
+        System.out.println("Altura: " + p.altura);
+        System.out.println("Raça: " + p.raca);
+        System.out.println("Data de Nascimento: " + p.datanasc);
+        System.out.println("Nacionalidade: " + p.nacionalidade);
+        System.out.println("RG: " + p.RG);
+        System.out.println("Telefone: " + p.telefone);
+        System.out.println("Email: " + p.email);
+    
+        System.out.println("Nome: " + p1.nome);
+        System.out.println("Idade: " + p1.idade);
+        System.out.println("Renda:" + p1.renda);
+        System.out.println("CPF: " + p1.cpf);
+        System.out.println("Endereço: " + p1.endereco);
+        System.out.println("Peso: " + p1.peso);
+        System.out.println("Altura: " + p1.altura);
+        System.out.println("Raça: " + p1.raca);
+        System.out.println("Data de Nascimento: " + p1.datanasc);
+        System.out.println("Nacionalidade: " + p1.nacionalidade);
+        System.out.println("RG: " + p1.RG);
+        System.out.println("Telefone: " + p1.telefone);
+        System.out.println("Email: " + p1.email);
+
+
+
+
+        Time t;
+        t = new Time();
+        Time t1 = new Time();
+
+        t.time = "Flamengo";
+        t.cidade = "Rio de Janeiro";
+        t.estado = "RJ";
+        t.pais = "Brasil";
+        t.tecnico = "Rogério Ceni";
+        t.presidente = "Rodolfo Landim";
+        t.fundacao = "15/11/1895";
+        t.estadio = "Maracanã";
+        t.cores = "Vermelho e Preto";
+        t.mascote = "Urubu";
+        
+        System.out.println("Time: " + t.time);
+        System.out.println("Cidade: " + t.cidade);
+        System.out.println("Estado: " + t.estado);
+        System.out.println("País: " + t.pais);
+        System.out.println("Técnico: " + t.tecnico);
+        System.out.println("Presidente: " + t.presidente);
+        System.out.println("Fundação: " + t.fundacao);
+        System.out.println("Estádio: " + t.estadio);
+        System.out.println("Cores: " + t.cores);
+        System.out.println("Mascote: " + t.mascote);
+
+        System.out.println("Time: " + t1.time);
+        System.out.println("Cidade: " + t1.cidade);
+        System.out.println("Estado: " + t1.estado);
+        System.out.println("País: " + t1.pais);
+        System.out.println("Técnico: " + t1.tecnico);
+        System.out.println("Presidente: " + t1.presidente);
+        System.out.println("Fundação: " + t1.fundacao);
+        System.out.println("Estádio: " + t1.estadio);
+        System.out.println("Cores: " + t1.cores);
+        System.out.println("Mascote: " + t1.mascote);
+        
+
+
+        Campeonato c;
+        c = new Campeonato();
+        Campeonato c1 = new Campeonato();
+
+        c.nome = "Brasileirão";
+        c.pais = "Brasil";
+        c.continente = "América do Sul";
+        c.premio = "R$ 18.000.000,00";
+        c.datainicio = "29/05/2024";
+        c.datafim = "05/12/2024";
+        c.patrocinador = "Banco do Brasil";
+        c.vice = "Flamengo";
+        c.campeao = "São Paulo";
+        c.artilheiro = "Gabigol";
+        
+
+        System.out.println("Nome: " + c.nome);
+        System.out.println("País: " + c.pais);
+        System.out.println("Continente: " + c.continente);
+        System.out.println("Prêmio: " + c.premio);
+        System.out.println("Data de Início: " + c.datainicio);
+        System.out.println("Data de Fim: " + c.datafim);
+        System.out.println("Patrocinador: " + c.patrocinador);
+        System.out.println("Vice: " + c.vice);
+        System.out.println("Campeão: " + c.campeao);
+        System.out.println("Artilheiro: " + c.artilheiro);
+        
+        System.out.println("Nome: " + c1.nome);
+        System.out.println("País: " + c1.pais);
+        System.out.println("Continente: " + c1.continente);
+        System.out.println("Prêmio: " + c1.premio);
+        System.out.println("Data de Início: " + c1.datainicio);
+        System.out.println("Data de Fim: " + c1.datafim);
+        System.out.println("Patrocinador: " + c1.patrocinador);
+        System.out.println("Vice: " + c1.vice);
+        System.out.println("Campeão: " + c1.campeao);
+        System.out.println("Artilheiro: " + c1.artilheiro);
+
+    }
+}
+    
